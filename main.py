50e2cca5d7b8b07a4a6577461668764edb04db3d
earlinn <earlinndrow@gmail.com>
2024-03-13 23:41:39 +0500

    Add classes for IO validation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(title="Trading App")
 
@@ -6,6 +11,14 @@ fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {
+        "id": 4,
+        "role": "investor",
+        "name": "Homer",
+        "degree": [
+            {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+        ],
+    },
 ]
 
 fake_users2 = [
@@ -34,14 +47,27 @@ fake_trades = [
 ]
 
 
-@app.get("/users/{user_id}")  # user_id is a path parameter (pass in URL)
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[list[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=list[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
-
-
-@app.get("/trades")  # limit and offset are query parameters (pass in URL after ?)
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
 
 
 @app.post("/users/{user_id}")
@@ -49,3 +75,23 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+@app.get("/trades")
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: list[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
