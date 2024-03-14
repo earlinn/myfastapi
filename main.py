e307975e9de8335553310cd3dc5f49d38b719ad1
earlinn <earlinndrow@gmail.com>
2024-03-14 18:42:10 +0500

    Create tasks database using sqlalchemy orm and make 2 endpoints

--- main.py
+++ main.py
@@ -1,109 +1,20 @@
-from datetime import datetime
-from enum import Enum
-from typing import Optional
+from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request, status
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import ResponseValidationError
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
 
-app = FastAPI(title="Trading App")
+from database import create_tables, delete_tables
+from router import router as tasks_router
 
 
-@app.exception_handler(ResponseValidationError)
-async def validation_exception_handler(request: Request, exc: ResponseValidationError):
-    return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"detail": exc.errors()}),
-    )
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("DB dropped")
+    await create_tables()
+    print("DB ready to work")
+    yield
+    print("Swith off")
 
 
-fake_users = [
-    {"id": 1, "role": "admin", "name": ["Bob"]},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-    {
-        "id": 4,
-        "role": "investor",
-        "name": "Homer",
-        "degree": [
-            {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
-        ],
-    },
-]
-
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-fake_trades = [
-    {
-        "id": 1,
-        "user_id": 1,
-        "currency": "BTC",
-        "side": "buy",
-        "price": 123,
-        "amount": 2.12,
-    },
-    {
-        "id": 2,
-        "user_id": 1,
-        "currency": "BTC",
-        "side": "sell",
-        "price": 125,
-        "amount": 2.12,
-    },
-]
-
-
-class DegreeType(Enum):
-    newbie = "newbie"
-    expert = "expert"
-
-
-class Degree(BaseModel):
-    id: int
-    created_at: datetime
-    type_degree: DegreeType
-
-
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    degree: Optional[list[Degree]] = []
-
-
-@app.get("/users/{user_id}", response_model=list[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
-
-
-@app.post("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
-
-
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
-
-
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: float = Field(ge=0)
-    amount: float
-
-
-@app.post("/trades")
-def add_trades(trades: list[Trade]):
-    fake_trades.extend(trades)
-    return {"status": 200, "data": fake_trades}
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
---
+++ router.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from repository import TaskRepository
+from schemas import STaskAdd
+
+router = APIRouter(prefix="/tasks", tags=["tasks"])
+
+
+@router.get("")
+async def get_tasks():
+    tasks = await TaskRepository.find_all()
+    return {"tasks": tasks}
+
+
+@router.post("")
+async def add_task(task: Annotated[STaskAdd, Depends()]):
+    task_id = await TaskRepository.add_one(task)
+    return {"ok": True, "task_id": task_id}
